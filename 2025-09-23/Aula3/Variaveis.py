6e5d8c4e7dbd20168d92f3dac73f31554916ba6d
felippeswat <felippe_fab@hotmail.com>
2025-09-23 22:00:20 -0300

    variaveis

--- 2025-09-23/Aula3/Variaveis.py
+++ 2025-09-23/Aula3/Variaveis.py
@@ -1,9 +1,15 @@
-ano_atual=2025
-ano_nascimento=1986
+ano_atual = 2025
+ano_nascimento = int(input('Em que ano você nasceu? '))
 
 
 idade=ano_atual-ano_nascimento
 
 #print(idade)
 
-print('Você tem ',idade, 'anos.')
+#print('Você tem ',idade, 'anos.')
+
+#print('Você tem '+str(idade)+ 'anos.')
+
+#print(f'Você tem {idade} anos.')
+
+print(f'Você masceu em {ano_nascimento} e tem {idade} anos.')
