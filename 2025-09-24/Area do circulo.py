53db1a6a45ad5e562c07495ee640391e77800600
felippeswat <felippe_fab@hotmail.com>
2025-09-24 20:57:54 -0300

    area do circulo

---
+++ 2025-09-24/Area do circulo.py
@@ -0,0 +1,7 @@
+print('Vamos calcular a área de um círculo!')
+
+raio        =int(input('Informe o raio do círculo a ser calculado: '))
+
+area        =3,1415926536*(raio**2)
+
+print (f'A área do circulo é: {area}')
