8810b6a6e879c663237a238a30ef338eb169c582
felippeswat <felippe_fab@hotmail.com>
2025-09-24 21:13:19 -0300

    Área do circulo

--- 2025-09-24/Area do circulo.py
+++ 2025-09-24/Area do circulo.py
@@ -4,7 +4,7 @@ from cmath import pi
 print('Vamos calcular a área de um círculo!')
 
 raio        =float(input('Informe o raio do círculo a ser calculado: '))
-pi          =3,1415926536
-area        =pi*(raio**2)
+pi          =3.1415926536
+area        =pi * (raio ** 2)
 
 print (f'A área do circulo é: {area}')
