ecfc9157d61fdf8cbd2131ab729e6812dd45d64b
felippeswat <felippe_fab@hotmail.com>
2025-09-28 20:06:59 -0300

    Velocidade média

---
+++ 2025-09-28/Velocidade média.py
@@ -0,0 +1,8 @@
+print('Vamos calcular a velocidade média do seu deslocamento.')
+
+distancia       =float(input('Digite a distância percorrida em KM: '))
+tempo           =float(input('Digite o tempo gasto em horas: '))
+
+velocidade       =distancia/tempo
+
+print(f'Sua velocidade média foi de:  {velocidade} Km/h')
