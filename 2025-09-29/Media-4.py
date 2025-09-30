14ac392432d14b2ad7b6ee098fa72490a4dffe84
felippeswat <felippe_fab@hotmail.com>
2025-09-30 20:56:25 -0300

    Media

---
+++ 2025-09-29/Media-4.py
@@ -0,0 +1,18 @@
+from msilib.schema import Media
+
+
+print('Vamos calcular a média de um aluno do IFRN no segundo semestre de 2025')
+
+nota1       =int(input('Informe a nota da ETAPA1: '))
+nota2       =int(input('Informe a nota da ETAPA2: '))
+
+media       =round((nota1*2 + nota2*3)/5)
+
+print(f'Nota da etapa 1: {nota1}')
+print(f'Nota da etapa 2: {nota2}')
+print(f'Média: {media}')
+
+if (media>=60):
+    print('APROVADO')
+else
+    print(' REPROVADO')
