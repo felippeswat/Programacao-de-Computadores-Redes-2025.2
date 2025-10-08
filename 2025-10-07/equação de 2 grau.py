6f0fc09b9ff07dee66af904a4f7b7a7ec13d0ba3
felippeswat <felippe_fab@hotmail.com>
2025-10-07 21:23:55 -0300

    Equação de segundo grau

---
+++ 2025-10-07/equação de 2 grau.py
@@ -0,0 +1,13 @@
+from re import A
+import sys
+
+
+print('Vamos calcular o valor das raizes da equação de 2º grau')
+
+a       =int(input('Informe o valor de "a" na equação: '))
+b       =int(input('Informe o valor de "b" na equação: '))
+c       =int(input('Informe o valor de "c" na equação: '))
+
+print(f'a: {a}')
+print(f'b: {b}')
+print(f'c: {c}')
