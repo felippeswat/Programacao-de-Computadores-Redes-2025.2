a81623d84ca28207f8db40477affa66374975c7b
felippeswat <felippe_fab@hotmail.com>
2025-10-14 21:04:00 -0300

    ano bissexto

---
+++ 2025-10-14/ano bissexto.py
@@ -0,0 +1,37 @@
+'''
+   Escreva um programa que pede ao usuário para inserir um ano e 
+   determina se ele é bissexto ou não. 
+   
+   Um ano é bissexto se atender a uma das seguintes regras:
+
+      - É divisível por 4, mas não é divisível por 100.
+
+      - É divisível por 400.
+
+      (Por exemplo, 2000 e 2400 são bissextos; 1800, 1900 e 2100 não são). 
+      
+   O programa deve exibir "O ano [ano] é bissexto." ou 
+   "O ano [ano] não é bissexto.". 
+   
+   Use try...except para validar a entrada.
+'''
+import sys
+
+# Entrada de dados com tratamento de erro
+try:
+   
+   ano     =   int(input('Informe o ano com 4 digitos: '))
+
+# Tratamento de erro para valores não numéricos
+except ValueError:
+   
+   sys.exit('ERRO: Você deve digitar um valor numérico e inteiro.')
+
+#Verificando se o ano é bissexto
+if ano % 100 == 0:
+
+    print(f'o ano de {ano} é bissexto!')
+
+else:
+
+    print(f'o ano de {ano} não é bissexto!')
