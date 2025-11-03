745cbc152c9a744bdce23390e52d3972f58e6979
felippeswat <felippe_fab@hotmail.com>
2025-11-03 19:29:40 -0300

    for

--- 2025-11-03/fatorial_for.py
+++ 2025-11-03/fatorial_for.py
@@ -28,7 +28,7 @@ else:
     intFatorial = 1
 
 # Usando for com range(intNumero, 1, -1).
-    for i in range(intNumero, 1, -1):
+    for intauxiliar in range(intNumero, 1, -1):
         intFatorial *= i
 
     print(f'{intNumero}! = {intFatorial}')
