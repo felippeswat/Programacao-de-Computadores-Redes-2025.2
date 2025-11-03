9c71c2a196285b89086639a6deb5a6ee2d36c1fe
felippeswat <felippe_fab@hotmail.com>
2025-11-03 17:49:10 -0300

    fatorial for

---
+++ 2025-11-03/fatorial(for).py
@@ -0,0 +1,34 @@
+'''vamos desenvolver um código para calcular o faturial de um numero fornecido pelo usuário'''
+
+# Importando a biblioteca sys.
+import sys
+
+# Entrada de dados do usuário com tratamento de erro.
+try:
+
+    intNumero = int(input('Informe um valor inteiro: '))
+
+# Tratamento de erro pra valores não numéricos.
+except ValueError:
+    sys.exit('ERRO: O valor informado deve ser inteiro...')
+
+# Tratamento para outros tipos de erro.
+except:
+    sys.exit(f'ERRO: {sys.exc_info()}')
+
+# Verificando se o valor da entrada é possitivo.
+else:
+    if intNumero < 0:
+        sys.exit('ERRO: Não existe fatorial de número negativo...')
+
+# Verificando se o valor da entrada é zero ou um.
+    if intNumero == 0 or intNumero == 1:
+        sys.exit(f'{intNumero}! = 1')
+
+    intFatorial = 1
+
+# Usando for com range(intNumero, 1, -1)
+    for i in range(intNumero, 1, -1):
+        intFatorial *= i
+
+    print(f'{intNumero}! = {intFatorial}')
