53836901287640fce36cbd7edf884586e34c97d9
felippeswat <felippe_fab@hotmail.com>
2025-11-12 21:40:56 -0300

    cont palavras

---
+++ 2025-11-12/contar palavras1.py
@@ -0,0 +1,12 @@
+'''Vamos contar quantas palavras temos em uma entrada digitada pelo usário'''
+
+strTexto        =       input('Digitem uma frase: ').strip()
+
+cont            =       1
+
+for espaco in strTexto:
+
+    if espaco == " ":
+        cont +=1
+
+print(cont)
