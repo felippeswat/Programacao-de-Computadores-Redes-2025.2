477718a2b1f670bcacd6e5ccd93606411155c261
felippeswat <felippe_fab@hotmail.com>
2025-11-12 21:04:23 -0300

    vogias

---
+++ 2025-11-12/contar vogais.py
@@ -0,0 +1,12 @@
+'''Vamos contar quantas vogais temos em uma palavra digitada pelo usário'''
+
+strTexto        =       input('Digitem uma palavra')
+
+vogais      =       "aeiou"
+cont        =       0
+
+for palavra in strTexto.lower():
+    if palavra in vogais:
+        cont +=1
+
+print(cont)
