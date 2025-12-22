0efc5df128222e555dfa75ede50d61a91fbc49ee
felippeswat <felippe_fab@hotmail.com>
2025-12-22 10:38:17 -0300

    capitais

--- 2025-12-17/capitais.py
+++ 2025-12-17/capitais.py
@@ -1,1 +1,26 @@
-'''Usar set() e usar filter()'''
+'''Fazer um programa que leia o arquivo capitais_brasil.csv e
+   preencha uma lista com sublistas contendo o nome da capital, 
+   a sigla do seu estado, a sigla da sua região e a sua população.
+
+   Após gerar a lista, gere uma lista contendo a sigla da região
+   e o total da população das capitais daquela região.
+
+   Em seguida salve o resultado em um arquivo chamado populacao_regioes.csv,
+   no mesmo diretório onde se encontra o programa, no seguinte formato 
+   (os valores abaixo são apenas ilustrativos):
+
+   Região;População
+   N;123456
+   NE;234567
+   CO;345678
+   S;456789
+   SE;567890
+   
+   Não use bibliotecas para manipulação de arquivos CSV.
+   Usar set() e usar filter()'''
+
+import sys, os
+
+strDiretorio = os.path.dirname(__file__)
+
+strNomeArquivo = capitais_brasil.csv
