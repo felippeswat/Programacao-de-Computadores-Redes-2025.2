28529fed931a2cef1f6cab0f1f466a4bd1d1edd5
felippeswat <felippe_fab@hotmail.com>
2026-02-02 20:02:05 -0300

    dict

---
+++ 2026-01-28/cursos_if.py
@@ -0,0 +1,14 @@
+'''
+   Usando o arquivo relacao_cursos_ifrn.json, faça o que se pede:
+
+   a) Leia o arquivo e armazene o conteúdo em uma variável do tipo dict (dictCursos)
+
+   b) Usando a função FILTER() com a função LAMBDA, um novo dicionário
+      (dictCursosModalidade) contendo apenas os cursos de 'Modalidade' igual a
+      'Tecnologia';
+
+   c) Ordene esse novo dicionário (dictCursosModalidade) pela chave 'NomeCurso';
+
+   d) Por fim, salve esse novo dicionário em um arquivo JSON chamado
+   cursos_tecnologia_ifrn.json
+'''
